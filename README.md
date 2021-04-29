48867d7b48dfdad21899362b87775b543260830f
jagadishchakma <jagadishchakma401@gmail.com>
2021-04-29 07:14:23 -0700

    bd transportation

---
+++ README.md
@@ -0,0 +1,21 @@
+# BD Transportation
+
+Websites Features:
+* Transportation Ticket System
+* Authentication
+  * Google
+  * Facebook
+  * Users Signup
+
+Technology Uses:
+* HTML
+* CSS
+* JavaScript
+* Reactjs
+* React-bootstrap
+* Bootstrap
+* Firebase Auth
+* Firebase Hosting
+
+
+ # [See Live Links Prevew](https://bd-transportation-81a2d.web.app/)
---
+++ src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData/fakeData.json';
+import Vehicle from '../Vehicle/Vehicle';
+import './Home.css';
+
+const Home = () => {
+    const [vehicles, setvehicles] = useState(fakeData);
+    document.title = "BD Transportation";
+    return (
+       <div className="home">
+            <div className="row">
+                {
+                    vehicles.map(data => <Vehicle vehicle={data} key={data.id}></Vehicle>)
+                }
+            </div>
+        </div>    
+    );
+};
+
+export default Home;
